44db1ba103c81ddd9801cc3787f1be8da068de2c
Altaiir1 <lemi25yuerekli@gmail.com>
2026-04-16 21:46:22 +0200

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <cstdio>
+
+int main()
+{
+	if (!glfwInit())
+	{
+		std::printf("Failed to initialize GLFW\n");
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Engine", nullptr, nullptr);
+	if (!window)
+	{
+		std::printf("Failed to create Window!\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::printf("Failed to initialize GLAD\n");
+		return 1;
+	}
+
+	std::printf("OpenGL %s\n", (const char*)glGetString(GL_VERSION));
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window); // PResent the rendered frame
+		glfwPollEvents();		 // Process Keyboard / mouse / window events
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
