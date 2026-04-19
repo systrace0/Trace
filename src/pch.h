45730483b08da0909a3d6adb8ea576361cddd694
Altaiir1 <lemi25yuerekli@gmail.com>
2026-04-19 22:50:17 +0200

    [REF]: Refactor root CMakeLists  and add src own CMakeLists for clear ownership and separation and add pch.h for precompiled headers used in most files

---
+++ src/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// OpenGL
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+// C++ standard - used broadly across the project
+#include <string>
+#include <cstdint>
+#include <cstdio>
