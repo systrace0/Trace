8748ddecd5c80836a2c756ac1ea229335bd2267d
Altaiir1 <lemi25yuerekli@gmail.com>
2026-04-28 00:04:58 +0200

    [WIP]: Setup basic ImGUiManager header as a foundation

--- src/imgui/ImGuiManager.h
+++ src/imgui/ImGuiManager.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include "Panel.h"
+#include "Window.h"
+
+#include <vector>
+#include <memory>
+
+namespace engine
+{
+	class ImGuiManager
+	{
+	public:
+		ImGuiManager(Window& window);
+		~ImGuiManager();
+
+		void beginFrame();
+		void endFrame();
+
+		// TODO: Look up unique_ptr on cppreference
+		void addPanel(std::unique_ptr<Panel> panel);
+
+	private:
+		std::vector<std::unique_ptr<Panel>> m_panels;
+	};
+}
