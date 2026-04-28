f763a03b4b72afb23ce46d0031e2f78f9bbe95a9
Altaiir1 <lemi25yuerekli@gmail.com>
2026-04-28 16:36:37 +0200

    [TASK]: Add vector & imgui includes to the pch.h since it is being used multiple times

--- src/pch.h
+++ src/pch.h
@@ -6,8 +6,11 @@
 
 // C++ standard - used broadly across the project
 #include <string>
+#include <vector>
 #include <cstdint>
 #include <cstdio>
+
+#include <imgui.h>
 
 // Custom header
 #include "Logger.h"
