829527277976f394c46de819943beed783e24840
Altaiir1 <lemi25yuerekli@gmail.com>
2026-04-28 20:15:12 +0200

    [FEAT]: Extend/Refactor DebugPanel with more and better debug information

--- src/imgui/panels/DebugPanel.h
+++ src/imgui/panels/DebugPanel.h
@@ -2,7 +2,8 @@
 
 #include "Panel.h"
 
-#include <imgui.h>
+#include <chrono>
+#include <array>
 
 namespace engine
 {
@@ -13,8 +14,23 @@ namespace engine
 		void draw() override;
 
 	private:
+		using Clock = std::chrono::steady_clock;
+		using TimePoint = Clock::time_point;
+
 		const char* m_glVersion{ nullptr };
 		const char* m_vendor{ nullptr };
 		const char* m_renderer{ nullptr };
+		const char* m_glsl{ nullptr };
+
+		static constexpr int kHistorySize = 128;
+
+		TimePoint m_lastFrame{ Clock::now() };
+		float m_frameTimeMs{ 0.0f };
+		float m_fpsSmoothed{ 0.0f };
+		float m_frameTimeMin{ 9999.0f };
+		float m_frameTimeMax{ 0.0f };
+		int m_historyOffset{ 0 };
+		bool m_showDemo{ false };
+		std::array<float, kHistorySize> m_frameTimeHistory{};
 	};
 }
