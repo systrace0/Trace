0991cef16928aef010872e1dced5c78579f01b2c
Altaiir1 <lemi25yuerekli@gmail.com>
2026-04-29 20:36:45 +0200

    [FEAT]: Add Google Test feature to learn and use google test in code

---
+++ tests/LoggerTests.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include "Logger.h"
+#include "imgui/panels/LogPanel.h"
+
+// TODO: Fix glad build error
+
+namespace trace
+{
+	TEST(LogPanel, PushAddsEntry)
+	{
+		trace::LogPanel::clear();
+		trace::LogPanel::push({ "test message", "12:00:00", trace::LogLevel::Info });
+
+		EXPECT_EQ(trace::LogPanel::entryCount(), 1);
+	}
+
+	TEST(LogPanel, RollingWindowCapsAt2000)
+	{
+		trace::LogPanel::clear();
+		for (int i = 0; i < 2500; i++)
+			trace::LogPanel::push({ "msg", "00:00:00", trace::LogLevel::Info });
+
+		EXPECT_LE(trace::LogPanel::entryCount(), 2000);
+	}
+
+	TEST(LogPanel, FilterLevelSkipsEntry)
+	{
+		trace::LogPanel::clear();
+		trace::LogPanel::push({ "debug msg", "00:00:00", trace::LogLevel::Debug });
+		trace::LogPanel::push({ "info msg",  "00:00:00", trace::LogLevel::Info });
+
+		EXPECT_EQ(trace::LogPanel::entryCount(), 2);
+	}
+
+} // namespace trace
